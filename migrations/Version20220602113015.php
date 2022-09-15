74639da67cdf7764e4f7d8cc4a6b2b74cdaa5a6c
voksenui <voksenui@gmail.com>
2022-09-15 09:43:04 +0300

    Revert "rwer"
    This reverts commit b60b06c97801145fa07ff137fc600953df77f748.

--- migrations/Version20220602113015.php
+++ migrations/Version20220602113015.php
@@ -19,8 +19,7 @@ final class Version20220602113015 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is a
-        ewrwarawerawerawerawuto-generated, please modify it to your needs
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE products_category (products_id INT NOT NULL, category_id INT NOT NULL, INDEX IDX_134D09726C8A81A9 (products_id), INDEX IDX_134D097212469DE2 (category_id), PRIMARY KEY(products_id, category_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE products_category ADD CONSTRAINT FK_134D09726C8A81A9 FOREIGN KEY (products_id) REFERENCES products (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE products_category ADD CONSTRAINT FK_134D097212469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
